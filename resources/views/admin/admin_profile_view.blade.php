cfeda609416aec54e3966637c9809ea658f39337
Amir2417 <amirsohelto@gmail.com>
2022-06-05 02:42:00 +0600

    admin profile page loaded

---
+++ resources/views/admin/admin_profile_view.blade.php
@@ -0,0 +1,13 @@
+@extends('admin.admin_master')
+@section('admin')
+<div class="container-full">
+
+		<!-- Main content -->
+		<section class="content">
+			<div class="row">
+                
+            </div>
+		</section>
+		<!-- /.content -->
+	  </div>
+@endsection
