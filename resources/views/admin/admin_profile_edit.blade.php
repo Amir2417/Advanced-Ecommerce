e9625128cf3a0d26e2e73b11f83e21f929f68394
Amir2417 <amirsohelto@gmail.com>
2022-06-05 03:57:43 +0600

    image upload automatically using javascript

--- resources/views/admin/admin_profile_edit.blade.php
+++ resources/views/admin/admin_profile_edit.blade.php
@@ -1,5 +1,6 @@
 @extends('admin.admin_master')
 @section('admin')
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <div class="container-full">
 			  
 
@@ -45,12 +46,12 @@
                                         <div class="form-group">
                                             <h5>Admin Profile Picture<span class="text-danger">*</span></h5>
                                             <div class="controls">
-                                                    <input type="file" name="profile_photo_path" class="form-control" required=""> 
+                                                    <input type="file" name="profile_photo_path" id="image" class="form-control" required=""> 
                                             </div>
                                         </div>
                                     </div>
                                     <div class="col-md-6">
-                                        <img src="{{ (!empty($editData->profile_photo_path))? url('upload/admin_images/'.$editData->profile_photo_path):url('upload/no_image.jpg') }}" style="width:100px; height:100px;" alt="">
+                                        <img id="showImage" src="{{ (!empty($editData->profile_photo_path))? url('upload/admin_images/'.$editData->profile_photo_path):url('upload/no_image.jpg') }}" style="width:100px; height:100px;" alt="">
                                     </div>
                                 </div>
                                 
@@ -79,4 +80,18 @@
 		</section>
 		<!-- /.content -->
 	  </div>
+
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $('#image').change(function(e){
+                var reader = new FileReader();
+                reader.onload = function(e){
+                    $('#showImage').attr('src',e.target.result);
+                }
+                reader.readAsDataURL(e.target.files['0']);
+            });
+        });
+    </script>
+
+
 @endsection
