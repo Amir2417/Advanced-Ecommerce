06f6d7b4eb96a957079e6fd2f29401cbfbfc56e0
Amir2417 <amirsohelto@gmail.com>
2022-06-07 15:06:44 +0600

    user profile section design

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,31 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-           Hi..{{ Auth::user()->name }}
-        </h2>
-    </x-slot>
+@extends('frontend.main_master')
+@section('content')
 
-    <div class="py-12">
-        This is just home page 
+<div class="body-content">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-2"><br>
+                <img class="card-img-top" style="border-radius:50%" src="{{ (!empty($editData->photo_profile_path))? url('upload/admin_images/'.$editData->photo_profile_path): url('upload/no_image.jpg')}}" height="100%" width="100%" alt=""><br><br>
+
+                <ul class="list-group list-group-flush" >
+                    <a class="btn btn-primary btn-sm btn-block" href="">Home</a>
+                    <a class="btn btn-primary btn-sm btn-block" href="">Profile Update</a>
+                    <a class="btn btn-primary btn-sm btn-block" href="">Change Password</a>
+                    <a class="btn btn-danger btn-sm btn-block" href="">Logout</a>
+                </ul>
+            </div>
+            <div class="col-md-2">
+                
+            </div>
+            <div class="col-md-6">
+                <div class="card">
+                    <h3 class="text-center"><span>Hi... <strong>{{Auth::user()->name}}</strong> Welcome to SoftLight Shoop</span></h3>
+                </div>
+            </div>
+        </div>
     </div>
-</x-app-layout>
+</div>
+
+
+
+@endsection
