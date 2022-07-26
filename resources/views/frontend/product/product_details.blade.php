de979f29b6cb92af7bdbb1e780cbb9188c827a40
Amir2417 <amirsohelto@gmail.com>
2022-07-26 15:30:58 +0600

    Add to cart partv completed in Product details page

--- resources/views/frontend/product/product_details.blade.php
+++ resources/views/frontend/product/product_details.blade.php
@@ -23,10 +23,10 @@
 				<div class="sidebar-module-container">
 				<div class="home-banner outer-top-n">
 <img src="assets/images/banners/LHS-banner.jpg" alt="Image">
-</div>		
-  
-    
-    
+</div>
+
+
+
     	<!-- ============================================== HOT DEALS ============================================== -->
 <div class="sidebar-widget hot-deals wow fadeInUp outer-top-vs">
 	<h3 class="section-title">hot deals</h3>
@@ -38,8 +38,8 @@
 							<div class="image">
 								<img src="{{ asset($product->product_thambnail) }}" alt="">
 							</div>
-							
-							@php 
+
+							@php
                               $amount = $product->selling_price - $product->discount_price;
                               $discount = ($amount/$product->selling_price) * 100;
 							@endphp
@@ -55,7 +55,7 @@
 										<span class="value">Days</span>
 									</div>
 								</div>
-				                
+
 				                <div class="box-wrapper">
 									<div class="hour box">
 										<span class="key">20</span>
@@ -88,30 +88,30 @@
                           @else
                           <div class="product-price"> <span class="price"> $ {{$product->discount_price}} </span> <span class="price-before-discount">$ {{$product->selling_price}}</span> </div>
                           @endif
-							
+
 						</div><!-- /.product-info -->
 
 						<div class="cart clearfix animate-effect">
 							<div class="action">
-								
+
 								<div class="add-cart-button btn-group">
 									<button class="btn btn-primary icon" data-toggle="dropdown" type="button">
-										<i class="fa fa-shopping-cart"></i>													
+										<i class="fa fa-shopping-cart"></i>
 									</button>
 									<button class="btn btn-primary cart-btn" type="button">Add to cart</button>
-															
+
 								</div>
-								
+
 							</div><!-- /.action -->
 						</div><!-- /.cart -->
-					</div>	
-				</div>		        
-			@endforeach		        
-						
-	    
+					</div>
+				</div>
+			@endforeach
+
+
     	</div><!-- /.sidebar-widget -->
 </div>
-<!-- ============================================== HOT DEALS: END ============================================== -->					
+<!-- ============================================== HOT DEALS: END ============================================== -->
 
 <!-- ============================================== NEWSLETTER ============================================== -->
 <div class="sidebar-widget newsletter wow fadeInUp outer-bottom-small outer-top-vs">
@@ -141,7 +141,7 @@
          <div class="item">
          	<div class="avatar"><img src="assets/images/testimonials/member3.png" alt="Image"></div>
 		<div class="testimonials"><em>"</em>Vtae sodales aliq uam morbi non sem lacus port mollis. Nunc condime tum metus eud molest sed consectetuer.<em>"</em></div>
-		<div class="clients_author">Stephen Doe	<span>Xperia Designs</span>	</div>    
+		<div class="clients_author">Stephen Doe	<span>Xperia Designs</span>	</div>
         </div><!-- /.item -->
 
         <div class="item">
@@ -152,17 +152,17 @@
 
     </div><!-- /.owl-carousel -->
 </div>
-    
+
 <!-- ============================================== Testimonials: END ============================================== -->
 
- 
+
 
 				</div>
 			</div><!-- /.sidebar -->
 			<div class='col-md-9'>
             <div class="detail-block">
 				<div class="row  wow fadeInUp">
-                
+
 					     <div class="col-xs-12 col-sm-6 col-md-5 gallery-holder">
     <div class="product-item-holder size-big single-product-gallery small-gallery">
 
@@ -173,14 +173,14 @@
                     <img class="img-responsive" alt="" src="{{asset($img->photo_name)}}" data-echo="{{asset($img->photo_name)}}" />
                 </a>
             </div><!-- /.single-product-gallery-item -->
-            @endforeach 
-            
+            @endforeach
+
 
         </div><!-- /.single-product-slider -->
 
 
         <div class="single-product-gallery-thumbs gallery-thumbs">
-        
+
             <div id="owl-single-product-thumbnails">
             @foreach($image as $img)
                 <div class="item">
@@ -188,20 +188,20 @@
                         <img class="img-responsive" width="85" alt="" src="{{asset($img->photo_name)}}" data-echo="{{asset($img->photo_name)}}" />
                     </a>
                 </div>
-                @endforeach 
-               
+                @endforeach
+
             </div><!-- /#owl-single-product-thumbnails -->
 
-            
+
 
         </div><!-- /.gallery-thumbs -->
 
     </div><!-- /.single-product-gallery -->
-</div><!-- /.gallery-holder -->        			
+</div><!-- /.gallery-holder -->
 					<div class='col-sm-6 col-md-7 product-info-block'>
 						<div class="product-info">
-							<h1 class="name">@if(session()->get('language') == 'bangla') {{$products->product_name_ban}} @else {{$products->product_name_en}} @endif</h1>
-							
+							<h1 class="name" id="pname">@if(session()->get('language') == 'bangla') {{$products->product_name_ban}} @else {{$products->product_name_en}} @endif</h1>
+
 							<div class="rating-reviews m-t-20">
 								<div class="row">
 									<div class="col-sm-3">
@@ -212,7 +212,7 @@
 											<a href="#" class="lnk">(13 Reviews)</a>
 										</div>
 									</div>
-								</div><!-- /.row -->		
+								</div><!-- /.row -->
 							</div><!-- /.rating-reviews -->
 
 							<div class="stock-container info-container m-t-10">
@@ -220,28 +220,28 @@
 									<div class="col-sm-2">
 										<div class="stock-box">
 											<span class="label">Availability :</span>
-										</div>	
+										</div>
 									</div>
 									<div class="col-sm-9">
 										<div class="stock-box">
 											<span class="value">In Stock</span>
-										</div>	
+										</div>
 									</div>
-								</div><!-- /.row -->	
+								</div><!-- /.row -->
 							</div><!-- /.stock-container -->
 
 							<div class="description-container m-t-20">
                             @if(session()->get('language') == 'bangla') {{$products->short_descp_ban}} @else {{$products->short_descp_en}} @endif
-								
+
 							</div><!-- /.description-container -->
 
 							<div class="price-container info-container m-t-20">
 								<div class="row">
-									
+
 
 									<div class="col-sm-6">
 										<div class="price-box">
-                                        @php 
+                                        @php
                               $amount = $products->selling_price - $products->discount_price;
                               $discount = ($amount/$products->selling_price) * 100;
                           @endphp
@@ -250,7 +250,7 @@
                                             @else
                                             <span class="price">{{$products ->discount_price}}</span>
 											<span class="price-strike">{{$products ->selling_price}}</span>
-                                            @endif 
+                                            @endif
 										</div>
 									</div>
 
@@ -272,30 +272,36 @@
 							</div><!-- /.price-container -->
 							<!--Start size and Color -->
 							<div class="row">
-									
+
 
 									<div class="col-sm-6">
 										<div class="form-group">
 											<label class="info-title control-label">Select Color <span>*</span></label>
-											<select class="form-control unicase-form-control selectpicker" style="display: none;">
+											<select id="color" class="form-control unicase-form-control selectpicker" style="display: none;">
 												<option selected="" disabled="">--Select Color--</option>
 												@foreach($product_color_en as $color)
 												<option value="{{$color}}">{{ ucwords($color)}}</option>
 												@endforeach
 											</select>
 										</div>
-								
+
 									</div>
 
 									<div class="col-sm-6">
 										<div class="form-group">
+                                            @if ($products->product_size_en == NULL)
+
+                                            @else
+
+
 											<label class="info-title control-label">Select Size <span>*</span></label>
-											<select class="form-control unicase-form-control selectpicker" style="display: none;">
+											<select id="size" class="form-control unicase-form-control selectpicker" style="display: none;">
 												<option>--Select Size--</option>
 												@foreach($product_size_en as $size)
 												<option value="{{$size}}">{{ ucwords($size) }}</option>
 												@endforeach
 											</select>
+                                            @endif
 										</div>
 									</div>
 
@@ -303,11 +309,11 @@
 <!--End size and Color -->
 							<div class="quantity-container info-container">
 								<div class="row">
-									
+
 									<div class="col-sm-2">
 										<span class="label">Qty :</span>
 									</div>
-									
+
 									<div class="col-sm-2">
 										<div class="cart-quantity">
 											<div class="quant-input">
@@ -315,29 +321,30 @@
 								                  <div class="arrow plus gradient"><span class="ir"><i class="icon fa fa-sort-asc"></i></span></div>
 								                  <div class="arrow minus gradient"><span class="ir"><i class="icon fa fa-sort-desc"></i></span></div>
 								                </div>
-								                <input type="text" value="1">
+								                <input type="text" id="qty" min="1" value="1">
 							              </div>
 							            </div>
 									</div>
+                                    <input type="hidden" id="product_id" value="{{ $products->id }}" min="1">
 
 									<div class="col-sm-7">
-										<a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart inner-right-vs"></i> ADD TO CART</a>
+										<button type="submit" onclick="addToCart()" class="btn btn-primary"><i class="fa fa-shopping-cart inner-right-vs" ></i> ADD TO CART</button>
 									</div>
 
-									
+
 								</div><!-- /.row -->
 							</div><!-- /.quantity-container -->
 
-							
 
-							
 
-							
+
+
+
 						</div><!-- /.product-info -->
 					</div><!-- /.col-sm-7 -->
 				</div><!-- /.row -->
                 </div>
-				
+
 				<div class="product-tabs inner-bottom-xs  wow fadeInUp">
 					<div class="row">
 						<div class="col-sm-3">
@@ -350,16 +357,16 @@
 						<div class="col-sm-9">
 
 							<div class="tab-content">
-								
+
 								<div id="description" class="tab-pane in active">
 									<div class="product-tab">
 										<p class="text">@if(session()->get('language') == 'bangla') {!!$product->long_descp_ban!!} @else {!!$product->long_descp_en!!} @endif</p>
-									</div>	
+									</div>
 								</div><!-- /.tab-pane -->
 
 								<div id="review" class="tab-pane">
 									<div class="product-tab">
-																				
+
 										<div class="product-reviews">
 											<h4 class="title">Customer Reviews</h4>
 
@@ -368,17 +375,17 @@
 													<div class="review-title"><span class="summary">We love this product</span><span class="date"><i class="fa fa-calendar"></i><span>1 days ago</span></span></div>
 													<div class="text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit.Aliquam suscipit."</div>
 																										</div>
-											
+
 											</div><!-- /.reviews -->
 										</div><!-- /.product-reviews -->
-										
 
-										
+
+
 										<div class="product-add-review">
 											<h4 class="title">Write your own review</h4>
 											<div class="review-table">
 												<div class="table-responsive">
-													<table class="table">	
+													<table class="table">
 														<thead>
 															<tr>
 																<th class="cell-label">&nbsp;</th>
@@ -388,7 +395,7 @@
 																<th>4 stars</th>
 																<th>5 stars</th>
 															</tr>
-														</thead>	
+														</thead>
 														<tbody>
 															<tr>
 																<td class="cell-label">Quality</td>
@@ -418,11 +425,11 @@
 													</table><!-- /.table .table-bordered -->
 												</div><!-- /.table-responsive -->
 											</div><!-- /.review-table -->
-											
+
 											<div class="review-form">
 												<div class="form-container">
 													<form role="form" class="cnt-form">
-														
+
 														<div class="row">
 															<div class="col-sm-6">
 																<div class="form-group">
@@ -442,7 +449,7 @@
 																</div><!-- /.form-group -->
 															</div>
 														</div><!-- /.row -->
-														
+
 														<div class="action text-right">
 															<button class="btn btn-primary btn-upper">SUBMIT REVIEW</button>
 														</div><!-- /.action -->
@@ -451,22 +458,22 @@
 												</div><!-- /.form-container -->
 											</div><!-- /.review-form -->
 
-										</div><!-- /.product-add-review -->										
-										
+										</div><!-- /.product-add-review -->
+
 							        </div><!-- /.product-tab -->
 								</div><!-- /.tab-pane -->
 
 								<div id="tags" class="tab-pane">
 									<div class="product-tag">
-										
+
 										<h4 class="title">Product Tags</h4>
 										<form role="form" class="form-inline form-cnt">
 											<div class="form-container">
-									
+
 												<div class="form-group">
 													<label for="exampleInputTag">Add Your Tags: </label>
 													<input type="email" id="exampleInputTag" class="form-control txt">
-													
+
 
 												</div>
 
@@ -494,7 +501,7 @@
 					<h3 class="section-title">
 					  Related Product </h3>
 					<div class="owl-carousel home-owl-carousel custom-carousel owl-theme outer-top-xs">
-					  
+
 					  @foreach($related_products as $product)
 					  <div class="item item-carousel">
 							  <div class="products">
@@ -502,7 +509,7 @@
 								  <div class="product-image">
 									<div class="image"> <a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug_en)}}"><img  src="{{ asset($product->product_thambnail) }}" alt=""></a> </div>
 									<!-- /.image -->
-									@php 
+									@php
 										$amount = $product->selling_price - $product->discount_price;
 										$discount = ($amount/$product->selling_price) * 100;
 									@endphp
@@ -510,31 +517,31 @@
 									<div class="tag new"><span>new</span></div>
 									@else
 									<div class="tag hot"><span>{{ round($discount)}} $</span></div>
-									@endif 
+									@endif
 								  </div>
 								  <!-- /.product-image -->
-								  
+
 								  <div class="product-info text-left">
 									<h3 class="name"><a href="{{ url('product/details/'.$product->id.'/'.$product->product_slug_en)}}">
 									  @if(session()->get('language') == 'bangla') {{$product->product_name_ban}} @else {{$product->product_name_en}} @endif
 									  </a></h3>
 									<div class="rating rateit-small"></div>
 									<div class="description"></div>
-									
+
 									@if($product->discount_price == NULL)
 									<div class="product-price"> <span class="price">$ {{$product->selling_price}} </span>  </div>
 									@else
 									<div class="product-price"> <span class="price"> $ {{$product->discount_price}} </span> <span class="price-before-discount">$ {{$product->selling_price}}</span> </div>
 									@endif
-									
-									
-									
-									
-								   
-		  
-									
-									<!-- /.product-price --> 
-									
+
+
+
+
+
+
+
+									<!-- /.product-price -->
+
 								  </div>
 								  <!-- /.product-info -->
 								  <div class="cart clearfix animate-effect">
@@ -548,25 +555,25 @@
 										<li class="lnk"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html" title="Compare"> <i class="fa fa-signal" aria-hidden="true"></i> </a> </li>
 									  </ul>
 									</div>
-									<!-- /.action --> 
+									<!-- /.action -->
 								  </div>
-								  <!-- /.cart --> 
+								  <!-- /.cart -->
 								</div>
-								<!-- /.product --> 
-								
+								<!-- /.product -->
+
 							  </div>
-							  <!-- /.products --> 
+							  <!-- /.products -->
 							</div>
 							<!-- /.item -->
 							@endforeach
-					  
-					  
-					  <!-- /.item --> 
+
+
+					  <!-- /.item -->
 					</div>
-					<!-- /.home-owl-carousel --> 
+					<!-- /.home-owl-carousel -->
 				  </section>
 <!-- ============================================== UPSELL PRODUCTS : END ============================================== -->
-			
+
 			</div><!-- /.col -->
 			<div class="clearfix"></div>
 		</div><!-- /.row -->
