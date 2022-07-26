9d76a4343547ebd993439a8b195d2c82317ddcc7
Amir2417 <amirsohelto@gmail.com>
2022-07-26 16:09:59 +0600

    Data display in the cart section

--- resources/views/frontend/body/header.blade.php
+++ resources/views/frontend/body/header.blade.php
@@ -1,31 +1,31 @@
-<header class="header-style-1"> 
-  
+<header class="header-style-1">
+
   <!-- ============================================== TOP MENU ============================================== -->
   <div class="top-bar animate-dropdown">
     <div class="container">
       <div class="header-top-inner">
         <div class="cnt-account">
           <ul class="list-unstyled">
-            <li><a href="#"><i class="icon fa fa-user"></i>@if(session()->get('language') == 'bangla') 
+            <li><a href="#"><i class="icon fa fa-user"></i>@if(session()->get('language') == 'bangla')
             আমার প্রোফাইল @else My Profile @endif </a></li>
-            <li><a href="#"><i class="icon fa fa-heart"></i>@if(session()->get('language') == 'bangla') 
+            <li><a href="#"><i class="icon fa fa-heart"></i>@if(session()->get('language') == 'bangla')
             ইচ্ছেতালিকা @else Wishlist @endif</a></li>
-            <li><a href="#"><i class="icon fa fa-shopping-cart"></i>@if(session()->get('language') == 'bangla') 
+            <li><a href="#"><i class="icon fa fa-shopping-cart"></i>@if(session()->get('language') == 'bangla')
             আমার কার্ট @else My Cart @endif</a></li>
-            <li><a href="#"><i class="icon fa fa-check"></i>@if(session()->get('language') == 'bangla') 
+            <li><a href="#"><i class="icon fa fa-check"></i>@if(session()->get('language') == 'bangla')
             চেকআউট @else Checkout @endif</a></li>
            @auth
-           <li><a href="{{ route('login') }}"><i class="icon fa fa-user"></i>@if(session()->get('language') == 'bangla') 
+           <li><a href="{{ route('login') }}"><i class="icon fa fa-user"></i>@if(session()->get('language') == 'bangla')
            ব্যবহারকারীর প্রোফাইল @else User Profile @endif</a></li>
            @else
-           <li><a href="{{ route('login') }}"><i class="icon fa fa-lock"></i>@if(session()->get('language') == 'bangla') 
+           <li><a href="{{ route('login') }}"><i class="icon fa fa-lock"></i>@if(session()->get('language') == 'bangla')
            লগইন/রেজিস্টার করুন @else Login/Register @endif </a></li>
            @endauth
-            
+
           </ul>
         </div>
         <!-- /.cnt-account -->
-        
+
         <div class="cnt-block">
           <ul class="list-unstyled list-inline">
             <li class="dropdown dropdown-small"> <a href="#" class="dropdown-toggle" data-hover="dropdown" data-toggle="dropdown"><span class="value">USD </span><b class="caret"></b></a>
@@ -40,37 +40,37 @@
               <ul class="dropdown-menu">
                 @if(session()->get('language') == 'bangla')
                 <li><a href="{{ route('language.english') }}">English</a></li>
-                @else 
+                @else
                 <li><a href="{{ route('language.bangla') }}">বাংলা</a></li>
                 @endif
-                
+
               </ul>
             </li>
           </ul>
-          <!-- /.list-unstyled --> 
+          <!-- /.list-unstyled -->
         </div>
         <!-- /.cnt-cart -->
         <div class="clearfix"></div>
       </div>
-      <!-- /.header-top-inner --> 
+      <!-- /.header-top-inner -->
     </div>
-    <!-- /.container --> 
+    <!-- /.container -->
   </div>
-  <!-- /.header-top --> 
+  <!-- /.header-top -->
   <!-- ============================================== TOP MENU : END ============================================== -->
   <div class="main-header">
     <div class="container">
       <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-3 logo-holder"> 
+        <div class="col-xs-12 col-sm-12 col-md-3 logo-holder">
           <!-- ============================================================= LOGO ============================================================= -->
-          
+
           <div class="logo"> <a href="{{ url('/')}}"> <img src="{{ asset('frontend/assets/images/logo.png')}}" alt="logo"> </a> </div>
-          <!-- /.logo --> 
+          <!-- /.logo -->
           <!-- ============================================================= LOGO : END ============================================================= --> </div>
         <!-- /.logo-holder -->
-        
-        <div class="col-xs-12 col-sm-12 col-md-7 top-search-holder"> 
-          <!-- /.contact-row --> 
+
+        <div class="col-xs-12 col-sm-12 col-md-7 top-search-holder">
+          <!-- /.contact-row -->
           <!-- ============================================================= SEARCH AREA ============================================================= -->
           <div class="search-area">
             <form>
@@ -90,13 +90,13 @@
                 <a class="search-button" href="#" ></a> </div>
             </form>
           </div>
-          <!-- /.search-area --> 
+          <!-- /.search-area -->
           <!-- ============================================================= SEARCH AREA : END ============================================================= --> </div>
         <!-- /.top-search-holder -->
-        
-        <div class="col-xs-12 col-sm-12 col-md-2 animate-dropdown top-cart-row"> 
-          <!-- ============================================================= SHOPPING CART DROPDOWN ============================================================= -->
-          
+
+        <div class="col-xs-12 col-sm-12 col-md-2 animate-dropdown top-cart-row">
+          <!-- ====== SHOPPING CART DROPDOWN ======== -->
+
           <div class="dropdown dropdown-cart"> <a href="#" class="dropdown-toggle lnk-cart" data-toggle="dropdown">
             <div class="items-cart-inner">
               <div class="basket"> <i class="glyphicon glyphicon-shopping-cart"></i> </div>
@@ -106,57 +106,45 @@
             </a>
             <ul class="dropdown-menu">
               <li>
-                <div class="cart-item product-summary">
-                  <div class="row">
-                    <div class="col-xs-4">
-                      <div class="image"> <a href="detail.html"><img src="{{ asset('frontend/assets/images/cart.jpg')}}" alt=""></a> </div>
-                    </div>
-                    <div class="col-xs-7">
-                      <h3 class="name"><a href="index.php?page-detail">Simple Product</a></h3>
-                      <div class="price">$600.00</div>
-                    </div>
-                    <div class="col-xs-1 action"> <a href="#"><i class="fa fa-trash"></i></a> </div>
-                  </div>
+                <div id="miniCart">
+                    
                 </div>
-                <!-- /.cart-item -->
-                <div class="clearfix"></div>
-                <hr>
                 <div class="clearfix cart-total">
                   <div class="pull-right"> <span class="text">Sub Total :</span><span class='price'>$600.00</span> </div>
                   <div class="clearfix"></div>
                   <a href="checkout.html" class="btn btn-upper btn-primary btn-block m-t-20">Checkout</a> </div>
-                <!-- /.cart-total--> 
-                
+                <!-- /.cart-total-->
+
               </li>
             </ul>
-            <!-- /.dropdown-menu--> 
+            <!-- /.dropdown-menu-->
           </div>
-          <!-- /.dropdown-cart --> 
-          
+          <!-- /.dropdown-cart -->
+
           <!-- ============================================================= SHOPPING CART DROPDOWN : END============================================================= --> </div>
-        <!-- /.top-cart-row --> 
+        <!-- /.top-cart-row -->
       </div>
-      <!-- /.row --> 
-      
+      <!-- /.row -->
+
     </div>
-    <!-- /.container --> 
-    
+    <!-- /.container -->
+
   </div>
-  <!-- /.main-header --> 
-  
+  <!-- /.main-header -->
+
   <!-- ============================================== NAVBAR ============================================== -->
   <div class="header-nav animate-dropdown">
     <div class="container">
       <div class="yamm navbar navbar-default" role="navigation">
         <div class="navbar-header">
-       <button data-target="#mc-horizontal-menu-collapse" data-toggle="collapse" class="navbar-toggle collapsed" type="button"> 
+       <button data-target="#mc-horizontal-menu-collapse" data-toggle="collapse" class="navbar-toggle collapsed" type="button">
        <span class="sr-only">Toggle navigation</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </button>
         </div>
         <div class="nav-bg-class">
           <div class="navbar-collapse collapse" id="mc-horizontal-menu-collapse">
             <div class="nav-outer">
               <ul class="nav navbar-nav">
-                <li class="active dropdown yamm-fw"> <a href="{{ url('/') }}" data-hover="dropdown" class="dropdown-toggle" data-toggle="dropdown">@if(session()->get('language') == 'bangla') 
+                <li class="active dropdown yamm-fw"> <a href="{{ url('/') }}" data-hover="dropdown" class="dropdown-toggle" data-toggle="dropdown">@if(session()->get('language') == 'bangla')
                 বাড়ি @else Home @endif
                   </a> </li>
 
@@ -167,7 +155,7 @@
                 @endphp
                 @foreach($categories as $category)
                 <li class="dropdown yamm mega-menu"> <a href="home.html" data-hover="dropdown" class="dropdown-toggle" data-toggle="dropdown">
-                @if(session()->get('language') == 'bangla') 
+                @if(session()->get('language') == 'bangla')
                 {{ $category->category_name_ban }} @else {{ $category->category_name_en }} @endif</a>
                   <ul class="dropdown-menu container">
                     <li>
@@ -180,7 +168,7 @@
                           @endphp
                           @foreach($subcategories as $subcategory)
                           <div class="col-xs-12 col-sm-6 col-md-2 col-menu">
-                            <a href="{{ url('subcategory/product/'.$subcategory->id.'/'.$subcategory->subcategory_name_en)}}"><h2 class="title">@if(session()->get('language') == 'bangla') 
+                            <a href="{{ url('subcategory/product/'.$subcategory->id.'/'.$subcategory->subcategory_name_en)}}"><h2 class="title">@if(session()->get('language') == 'bangla')
                             {{ $subcategory->subcategory_name_ban }} @else {{ $subcategory->subcategory_name_en }} @endif </h2></a>
                             <!-- Get SubCategory Table Data  -->
                             @php
@@ -189,45 +177,45 @@
                             @endphp
                             @foreach($subsubcategories as $subsubcategory)
                             <ul class="links">
-                            <li><a href="{{ url('subsubcategory/product/'.$subsubcategory->id.'/'.$subsubcategory->subsubcategory_slug_en)}}">@if(session()->get('language') == 'bangla') 
+                            <li><a href="{{ url('subsubcategory/product/'.$subsubcategory->id.'/'.$subsubcategory->subsubcategory_slug_en)}}">@if(session()->get('language') == 'bangla')
                               {{ $subsubcategory->subsubcategory_name_ban}} @else {{ $subsubcategory->subsubcategory_name_en}} @endif
                               </a></li>
-                              
+
                             </ul>
                             @endforeach
                           </div>
                           @endforeach
                           <!-- /.col -->
-                          
-                         
+
+
                           <!-- /.col -->
-                          
+
                           <div class="col-xs-12 col-sm-6 col-md-4 col-menu banner-image"> <img class="img-responsive" src="{{ asset('frontend/assets/images/banners/top-menu-banner.jpg')}}" alt=""> </div>
-                          <!-- /.yamm-content --> 
+                          <!-- /.yamm-content -->
                         </div>
                       </div>
                     </li>
                   </ul>
                 </li>
-                @endforeach 
+                @endforeach
                 <li class="dropdown  navbar-right special-menu"> <a href="#">Todays offer</a> </li>
               </ul>
               <!-- /.navbar-nav -->
               <div class="clearfix"></div>
             </div>
-            <!-- /.nav-outer --> 
+            <!-- /.nav-outer -->
           </div>
-          <!-- /.navbar-collapse --> 
-          
+          <!-- /.navbar-collapse -->
+
         </div>
-        <!-- /.nav-bg-class --> 
+        <!-- /.nav-bg-class -->
       </div>
-      <!-- /.navbar-default --> 
+      <!-- /.navbar-default -->
     </div>
-    <!-- /.container-class --> 
-    
+    <!-- /.container-class -->
+
   </div>
-  <!-- /.header-nav --> 
-  <!-- ============================================== NAVBAR : END ============================================== --> 
-  
+  <!-- /.header-nav -->
+  <!-- ============================================== NAVBAR : END ============================================== -->
+
 </header>
