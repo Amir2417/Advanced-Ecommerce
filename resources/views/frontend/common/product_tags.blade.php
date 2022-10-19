14017723539d112eb777322dacd7847cbb172eea
Amir2417 <amirsohelto@gmail.com>
2022-10-19 21:15:28 +0600

    Checkout Page

--- resources/views/frontend/common/product_tags.blade.php
+++ resources/views/frontend/common/product_tags.blade.php
@@ -1,7 +1,7 @@
 @php
 $tags_en = App\Models\Product::groupBy('product_tags_en')->select('product_tags_en')->get();
 $tags_ban = App\Models\Product::groupBy('product_tags_ban')->select('product_tags_ban')->get();
-@endphp  
+@endphp
 
 
 <div class="sidebar-widget product-tag wow fadeInUp">
@@ -11,19 +11,19 @@ $tags_ban = App\Models\Product::groupBy('product_tags_ban')->select('product_tag
             @if(session()->get('language')=='bangla')
             @foreach($tags_ban as $tag)
             <a class="item active" href="{{ url('product/tag/'.$tag->product_tags_ban) }}">
-	            {{ str_replace(',',' ',$tag->product_tags_ban)  }}</a> 
+	            {{ str_replace('[],',' ',$tag->product_tags_ban)  }}</a>
             @endforeach
 
-            @else       
+            @else
 
             @foreach($tags_en as $tag)
             <a class="item active"  href="{{ url('product/tag/'.$tag->product_tags_en) }}">
-	        {{ str_replace(',',' ',$tag->product_tags_en)  }}</a> 
+	        {{ str_replace(',',' ',$tag->product_tags_en)  }}</a>
             @endforeach
- 
+
             @endif
         </div>
-            <!-- /.tag-list --> 
+            <!-- /.tag-list -->
     </div>
-          <!-- /.sidebar-widget-body --> 
+          <!-- /.sidebar-widget-body -->
 </div>
