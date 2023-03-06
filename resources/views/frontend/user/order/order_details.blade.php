7be01fc0229e7b8985d7cc94c93ae8273371b911
Amir2417 <amirsohelto@gmail.com>
2023-03-06 22:43:53 +0600

    Product info with order list

--- resources/views/frontend/user/order/order_details.blade.php
+++ resources/views/frontend/user/order/order_details.blade.php
@@ -6,7 +6,6 @@
     <div class="container">
         <div class="row">
             @include('frontend.common.use_sidebar')
-
             <div class="col-md-5">
                 <div class="card">
                     <div class="card-header"><h4>Shipping Details</h4></div>
@@ -47,6 +46,118 @@
                             </tr>
                         </table>
                     </div>
+                </div>
+            </div>
+            <div class="col-md-5">
+                <div class="card">
+                    <div class="card-header"><h4>Order Details For
+                        <span style="color:red">Invoice : {{ $order->invoice_no }}</span></h4>
+                    </div>
+                    <hr>
+                    <div class="card-body" style="background: #E9EBEC">
+                        <table class="table">
+                            <tr>
+                                <th>Name:</th>
+                                <th>{{ $order->user->name }}</th>
+                            </tr>
+                            <tr>
+                                <th>Email:</th>
+                                <th>{{ $order->user->email }}</th>
+                            </tr>
+                            <tr>
+                                <th>Phone:</th>
+                                <th>{{ $order->phone }}</th>
+                            </tr>
+                            <tr>
+                                <th>Payment Type:</th>
+                                <th>{{ $order->payment_type }}</th>
+                            </tr>
+                            <tr>
+                                <th>Payment Method :</th>
+                                <th>{{ $order->payment_method }}</th>
+                            </tr>
+                            <tr>
+                                <th>Tranx ID :</th>
+                                <th>{{ $order->transaction_id}}</th>
+                            </tr>
+                            <tr>
+                                <th>Order Total :</th>
+                                <th>{{ $order->amount }}</th>
+                            </tr>
+                            <tr>
+                                <th>Order :</th>
+                                <th><span class="badge badge-pill badge-warning" style="background: #418DB9">{{ $order->status }}</span></th>
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="table-responsive">
+                    <h3 class="text-center">Product List</h3>
+                    <hr>
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr style="background: #e2e2e2;">
+                                <td class="col-md-1">
+                                    <label for="date">Image</label>
+                                </td>
+                                <td class="col-md-4">
+                                    <label for="amount">Product Name</label>
+                                </td>
+                                <td class="col-md-1">
+                                    <label for="Method">Product Code</label>
+                                </td>
+                                <td class="col-md-1">
+                                    <label for="Invoice">Color</label>
+                                </td>
+                                <td class="col-md-1">
+                                    <label for="Order">Size</label>
+                                </td>
+                                <td class="col-md-1">
+                                    <label for="Action">Quantity</label>
+                                </td>
+                                <td class="col-md-1">
+                                    <label for="Action">Price</label>
+                                </td>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+
+                            @foreach ($orderItem as $item)
+                                <tr>
+                                    <td class="col-md-1">
+                                        <label for="date"><img src="{{ asset($item->product->product_thambnail) }}" width="50px;" height="50px;" alt="" srcset=""></label>
+                                    </td>
+                                    <td class="col-md-4">
+                                        <label for="amount">{{ $item->product->product_name_en }}</label>
+                                    </td>
+                                    <td class="col-md-1">
+                                        <label for="Method">{{ $item->product->product_code }}</label>
+                                    </td>
+                                    <td class="col-md-1">
+                                        <label for="Invoice">{{ $item->color }}</label>
+                                    </td>
+                                    <td class="col-md-1">
+                                        {{ $item->size }}
+                                    </td>
+                                    <td class="col-md-1">
+                                        {{ $item->qty }}
+                                    </td>
+                                    <td class="col-md-1">
+                                         ${{ $item->product->discount_price }}  (${{ $item->product->discount_price * $item->qty }})
+                                    </td>
+                                </tr>
+                            @endforeach
+
+                        </tbody>
+
+                    </table>
+
                 </div>
             </div>
         </div>
